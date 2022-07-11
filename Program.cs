d5b34304523d69d61134ec68ee1ffdc9c6959881
Eduardo Marçal Clemente <80399639+Marshal86@users.noreply.github.com>
2022-07-11 16:23:43 -0300

    Programa principal
    Ler os dados de um pedido com N itens (N fornecido pelo usuário). Depois, mostrar um 
    sumário do pedido conforme exemplo Nota: o instante do pedido deve ser 
    o instante do sistema: DateTime.Now
    
    
    Enter cliente data:
    Name: Alex Green
    Email: alex@gmail.com
    Birth date (DD/MM/YYYY): 15/03/1985
    Enter order data:
    Status: Processing
    How many items to this order? 2
    Enter #1 item data:
    Product name: TV
    Product price: 1000.00
    Quantity: 1
    Enter #2 item data:
    Product name: Mouse
    Product price: 40.00
    Quantity: 2
    ORDER SUMMARY:
    Order moment: 20/04/2018 11:25:09
    Order status: Processing
    Client: Alex Green (15/03/1985) - alex@gmail.com
    Order items:
    TV, $1000.00, Quantity: 1, Subtotal: $1000.00
    Mouse, $40.00, Quantity: 2, Subtotal: $80.00
    Total price: $1080.00

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Atividade9.Entities;
+using Atividade9.Entities.Enum;
+
+namespace Atividade9
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter cliente data: ");
+            Console.WriteLine();
+            Console.Write("Name ");
+            string nome = Console.ReadLine();
+            Console.Write("E-mail ");
+            string email = Console.ReadLine();
+            Console.Write("Birthday ");
+            DateTime aniversario = DateTime.Parse(Console.ReadLine());
+
+            Client cliente = new Client(nome, email, aniversario);
+
+            Console.WriteLine("Enter order data:");
+            Console.Write("Status: ");
+            OrderStatus os = Enum.Parse<OrderStatus>(Console.ReadLine());
+            Console.Write("How many items to this order ?");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Order pedido = new Order(DateTime.Now, os, cliente);
+
+
+            for (int i = 1; i<=quantidade; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data: ");
+                Console.Write("Produto ");
+                string pnome = Console.ReadLine();
+                Console.Write("Preço ");
+                double ppreco = double.Parse(Console.ReadLine());
+                Console.WriteLine("Quantidade");
+                int pquantidade = int.Parse(Console.ReadLine());
+
+                Product produto = new Product(pnome, ppreco);
+                OrderItem item = new OrderItem(pquantidade, ppreco, produto);
+                
+
+                pedido.AddItem(item);
+                
+
+            }
+
+            pedido.ToString();
+
+
+
+
+          
+
+        }
+    }
+}
