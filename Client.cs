4ceabca31e29b1168370d09df6fa928731b67636
Eduardo Marçal Clemente <80399639+Marshal86@users.noreply.github.com>
2022-07-11 16:29:48 -0300

    Add files via upload

---
+++ Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade9.Entities
+{
+    class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime Birtday { get; set; }
+
+        public Client()
+            {
+            }
+
+        public Client(string name, string email, DateTime birtday)
+        {
+            Name = name;
+            Email = email;
+            Birtday = birtday;
+        }
+    }
+}
---
+++ Order.cs
@@ -0,0 +1,67 @@
+﻿using Atividade9.Entities.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade9.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+
+        public List<OrderItem> Itens { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem itens)
+        {
+            Itens.Add(itens);
+        }
+
+        public void RemoveItem(OrderItem itens)
+        {
+            Itens.Remove(itens);
+        }
+
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("ORDER SUMMARY: ");
+            sb.Append("Order moment: ");
+            sb.Append(Moment);
+            sb.Append("Order status: ");
+            sb.Append(Status);
+            sb.Append("Client : ");
+            sb.Append(Client.Name);
+            sb.Append(Client.Birtday);
+
+            sb.Append("Order items: ");
+            
+            foreach (OrderItem o in Itens)
+            {
+
+                sb.Append(o.Product); sb.Append(o.Quantity); sb.Append(o.Price);
+            }
+            
+
+            return sb.ToString();
+        }
+
+
+       
+    }
+}
